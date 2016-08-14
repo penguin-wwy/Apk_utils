c955d1804776920ad3cf369a4194c434e1d4764b
penguin-wwy <940375606@qq.com>
2016-08-14 22:05:12 +0800

    first_4

--- apk_utils/file.py
+++ apk_utils/file.py
@@ -1,6 +1,15 @@
 import struct
 from binascii import unhexlify
 
+def printHex(arr):
+    res = ''
+    for i in range(0, len(arr)):
+        res = res + hex(arr[i]) + ' '
+    return res
+
+def toLong(arr):
+    return struct.unpack('L', bytes(arr))[0]
+
 class File:
     def __init__(self, filePath):
         self.__fileName  = filePath.split("\\")[-1]
@@ -21,19 +30,117 @@ class File:
     def getFilePath(self):
         return self.__filePath
 
+    def getRawBinary(self):
+        return self.__rawBinary
+
 class AroidManifest:
     def __init__(self, fileInfo):
         self.__fileInfo = fileInfo
+        self.strTable = []
 
     def analyze(self):
-        outFile = self.__fileInfo.__filePath + "out.xml"
+        outFile = self.__fileInfo.getFilePath().replace("AroidManifest", "out.xml")
         head = self.readHead()
-        print("Magic num: " + head[0])
-        print("File Size: " + struct.unpack('L', bytes(head[1]))[0])
+        print("Magic num: " + printHex(head[0]))
+        print("File Size: " + str(toLong(head[1])))
+
+        endOfStr = self.readStringChunk()
+        endOfR   = self.readResourceIdChunk(endOfStr)
+        endOfStartN = self.readStartNamespaceChunk(endOfR)
+        self.readStratTagChunk(endOfStartN)
+
 
 
     def readHead(self):
-        return [self.__fileInfo.__rawBinary[:4], self.__fileInfo.__rawBinary[4:8]]
+        return [self.__fileInfo.getRawBinary()[:4], self.__fileInfo.getRawBinary()[4:8]]
 
     def readStringChunk(self):
-        pass
+        chunkSize = self.__fileInfo.getRawBinary()[12:16]
+        chunkSize = toLong(chunkSize)
+        rawBinary = self.__fileInfo.getRawBinary()[8:chunkSize+8]
+        strCount = toLong(rawBinary[8:12])
+        offset = toLong(rawBinary[20:24]) + 8
+        offOfEachStr = []
+
+        print("String Chunk Type: " + printHex(rawBinary[0:4]))
+        print("String Chunk Size: " + str(chunkSize))
+        print("String Count: " + str(strCount))
+        print("Style Count: " + printHex(rawBinary[12:16]))
+        print("Unknow: " + printHex(rawBinary[16:20]))
+        print("String Start: " + str(hex(offset)))
+
+        for i in range(0, strCount):
+            offOfEachStr.append(rawBinary[28+4*i:32+4*i])
+
+        for i in range(0, strCount):
+            string = ''
+            startOffset = offset + toLong(offOfEachStr[i]) - 8
+            chrOffset = startOffset + 2
+            while rawBinary[chrOffset] != 0:
+                string += chr(rawBinary[chrOffset])
+                chrOffset += 2
+            print("Str: " + string)
+            self.strTable.append(string)
+
+        return chunkSize+8
+
+    def readResourceIdChunk(self, endOfStr):
+        chunkSize = self.__fileInfo.getRawBinary()[endOfStr+4:endOfStr+8]
+        chunkSize = toLong(chunkSize)
+        rawBinary = self.__fileInfo.getRawBinary()[endOfStr:endOfStr+chunkSize]
+
+        print("ResourceId Chunk Type: " + printHex(rawBinary[0:4]))
+        print("ResourceId Chunk Size: " + str(chunkSize))
+
+        count = (chunkSize - 8) // 4
+        for i in range(0, count):
+            id = toLong(rawBinary[8+i*4:12+i*4])
+            strHex = hex(id)
+            if len(strHex) != 10:
+                strHex = '0x' + (10-len(strHex)) * '0' + strHex[2:]
+                print("id: " + str(id) + " hex: " + strHex)
+
+        return endOfStr + chunkSize
+
+    def readStartNamespaceChunk(self, endOfR):
+        chunkSize = self.__fileInfo.getRawBinary()[endOfR+4:endOfR+8]
+        chunkSize = toLong(chunkSize)
+        rawBinary = self.__fileInfo.getRawBinary()[endOfR:endOfR+chunkSize]
+
+        print("Namespace Chunk Type: " + printHex(rawBinary[0:4]))
+        print("Namespace Chunk Size: " + str(chunkSize))
+
+        count = (chunkSize - 8) // 16
+        for i in range(0, count):
+            table = rawBinary[8+i*16:24+i*16]
+            lineNum = toLong(table[0:4])
+            prefix = toLong(table[8:12])
+            Uri = toLong(table[12:16])
+            print("Line Number: " + str(lineNum))
+            print("Prefix: " + str(prefix))
+            print("Prefix Str: " + self.strTable[prefix])
+            print("Uri: " + str(Uri))
+            print("Uri Str: " + self.strTable[Uri])
+
+        return endOfR + chunkSize
+
+    def readStratTagChunk(self, endOfStartN):
+        chunkSize = self.__fileInfo.getRawBinary()[endOfStartN+4:endOfStartN+8]
+        chunkSize = toLong(chunkSize)
+        rawBinary = self.__fileInfo.getRawBinary()[endOfStartN:endOfStartN+chunkSize]
+
+        print("Tag Chunk Type: " + printHex(rawBinary[0:4]))
+        print("Tag Chunk Size: " + str(chunkSize))
+
+        count = (chunkSize - 8) // 32
+        for i in range(0, 32):
+            table = rawBinary[8+i*32:40+i*32]
+            lineNum = table[0:4]
+            namespaceUri = table[8:12]
+            name = table[12:16]
+            flags= table[16:20]
+            AttributeCount = table[20:24]
+            ClassAtrribute = table[24:28]
+            Atrributes = table[28:32]
+
+            
